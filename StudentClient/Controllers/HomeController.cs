69381530ab2d161e804a364d9f7807723d1eb980
khanhSky <khanhSky@DESKTOP-6RFEK8Q>
2019-12-18 11:33:16 -0700

    create project StudentClient

---
+++ StudentClient/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentClient.ServiceReference1;
+
+namespace StudentClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IStudentService sv = new StudentServiceClient();
+            return View(sv.GetStudentList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
